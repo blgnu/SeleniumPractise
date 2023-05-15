4007185ef5970e8063eae89238fb5296792a5e28
bucar <bilgenucar@gmail.com>
2023-05-15 16:50:37 +0300

    testng group practise

--- src/test/java/TestNG.java
+++ src/test/java/TestNG.java
@@ -1,19 +1,19 @@
 import org.testng.annotations.Test;
 
 public class TestNG {
-    @Test
+    @Test (groups = {"Group A"})
     public void test1 () {
         System.out.println("test1");
     }
-    @Test
+    @Test (groups = {"Group B"})
     public void test2 () {
         System.out.println("test2");
     }
-    @Test
+    @Test (groups = {"Group A","Group B"})
     public void test3 () {
         System.out.println("test3");
     }
-    @Test
+    @Test (groups = {"Group C"})
     public void test4 () {
         System.out.println("test4");
     }
