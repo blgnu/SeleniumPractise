ca70a8a3f135b74d133b31e05456ef0dc2a0b6b5
bucar <bilgenucar@gmail.com>
2023-05-23 17:19:22 +0300

    test report practise

---
+++ TestNG3.java
@@ -0,0 +1,11 @@
+
+public class TestNG3 {
+    @Test (priority=1)
+    public void test1(){
+        System.out.printLn("test1");
+     @Test (priority=2)
+     public void test2(){
+         System.out.printLn("test2");
+
+    }
+}
